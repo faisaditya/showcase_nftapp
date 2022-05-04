b7edc7671ee3017505db44c49eef32a1a5fb24be
Faisaditya <faisadityaperdana@gmail.com>
2022-05-04 15:06:29 +0700

    Update 1

--- src/App.js
+++ src/App.js
@@ -7,27 +7,27 @@ const App = () => {
     <>
       <SectionWrapper
         title="You own store of Nifty NFTs. Start Selling & Growing"
-        description="Buy, Store, Collect NFTs exchange & earn crypto. Join 30+ million people using ProNef Marketplace"
+        description="Buy, store, collect NFTS, exchange & earn crypto. Join 25+ million people using ProNef Marketplace."
         showBtn
         mockupImg={assets.homeHero}
         banner="banner"
       />
       <SectionWrapper
         title="Smart User Interface Marketplace"
-        description="Experience a better UI of ProNef NFT Marketplace. Smooth constant colors of a fluent UI Design."
+        description="Experience a buttery UI of ProNef NFT Marketplace. Smooth constant colors of a fluent UI design."
         mockupImg={assets.homeCards}
         reverse
       />
       <Features />
       <SectionWrapper
         title="Deployment"
-        description="ProNef is built using Expo which runs natively on all users devices. You can easily get your app into people's hands"
+        description="ProNef is built using Expo which runs natively on all users' devices. You can easily get your app into people's hands"
         mockupImg={assets.feature}
         reverse
       />
       <SectionWrapper
         title="Creative way to showcase the store"
-        description="The app contain two screens. The first screen list all NFTs while the second one show the details of specific NFT"
+        description="The app contains two screens. The first screen lists all NFTs while the second one shows the details of a specific NFT."
         mockupImg={assets.mockup}
         banner="banner02"
       />
@@ -35,7 +35,8 @@ const App = () => {
 
       <div className="px-4 py-2 justify-center items-center bg-primary flex-col text-center banner04">
         <p className={`${styles.pText} ${styles.whiteText}`}>
-          Made by <span className="bold">Fais Aditya</span>
+          Made By
+          <span className="bold"> @faisaditya</span>
         </p>
       </div>
     </>
